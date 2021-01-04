44036d9d7436c6680f550c0ffd6ba0beb986a717
auooru <auooru@outlook.com>
2021-01-04 21:12:48 +0800

    update: 完善phar构建

--- run.php
+++ run.php
@@ -6,18 +6,25 @@ namespace SocketLog;
 use App\Server;
 use Phar;
 use Workerman\Worker;
+use function sprintf;
+use function str_replace;
 use const IN_PHAR;
 use const RUNNING_ROOT;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+const APP_VERSION = '@app-version@';
+const COMPILE_DATETIME = '@compile-datetime@';
+
 define('IN_PHAR', boolval(Phar::running(false)));
 define('RUNNING_ROOT', realpath(getcwd()));
 define('APP_ROOT', IN_PHAR ? Phar::running() : realpath(getcwd()));
 
-$unique_prefix = \str_replace('/', '_', __FILE__);
+$unique_prefix = str_replace('/', '_', __FILE__);
 Worker::$pidFile = "/tmp/w_{$unique_prefix}.pid";
 Worker::$logFile = RUNNING_ROOT . '/workerman.log';
+
+echo sprintf('App version: %s, Compile datetime: %s %s', APP_VERSION, COMPILE_DATETIME, PHP_EOL);
 
 new Server();
 
